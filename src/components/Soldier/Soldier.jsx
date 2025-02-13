a62e19b86f09f80477a49485fc0eac5a9a77207a
Amit Kushwaha <amitcoding56@gmail.com>
2025-02-13 16:24:02 +0530

    image add

--- src/components/Soldier/Soldier.jsx
+++ src/components/Soldier/Soldier.jsx
@@ -1,32 +1,22 @@
 import React from "react";
+import data from "../../../data.json";
 
 function Soldier() {
-  const soldiers = [
-    {
-      name: "Captain Saurabh Kalia",
-      rank: "Captain",
-      state: "(Gujrat)",
-    },
-    {
-      name: "Captain Nikesh Kalia",
-      rank: "Captain",
-      state: "(Bihar)",
-    },
-  ];
-
   return (
-    <div className="grid grid-cols-1 justify-center items-center gap-5 lg:grid-cols-3">
-      {soldiers.map((soldier, index) => (
+    <div className="grid grid-cols-1 justify-center items-center gap-5 sm:grid-cols-2 lg:grid-cols-3">
+      {data.map((soldier, index) => (
         <div
           className="flex flex-col justify-center items-center p-6 rounded-md border"
           key={index}
         >
+          <img
+            src={soldier.image}
+            alt={soldier.name}
+            className="rounded-md mb-4"
+          />
           <h3 className="text-2xl font-semibold text-white">{soldier.name}</h3>
-          <span className="text-white text-lg">{soldier.rank}</span>
+          <span className="text-white text-lg">{soldier.position}</span>
           <span className="text-white">{soldier.state}</span>
-          <button className="bg-white w-full py-2 px-8 text-lg rounded-md mt-4">
-            View
-          </button>
         </div>
       ))}
     </div>
